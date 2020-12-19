54af2a1fee2c395badf2ca026668a72b6e252c37
Chris Schopp <christopher.d.schopp@gmail.com>
2020-12-18 20:20:24 -0500

    added ws2, ws3, and step_b, step_c

--- SimPy/mes/factory_simulation.py
+++ SimPy/mes/factory_simulation.py
@@ -11,19 +11,33 @@ def lot_counter():
 
 lot_id = lot_counter()
 
+process_time_dist = {
+                    'STEP A': 2,
+                    'STEP B': 3,
+                    'STEP C': 4
+                    }
+
 
 class GlobalVariables:
     lot_status = pd.DataFrame(columns=['lot_id','step_name','step_arrival_time','process_start_time','process_end_time'])
 
 
 class Factory(object):
-    def __init__(self, env, num_machines_ws1):
+    def __init__(self, env, num_machines_ws1, num_machines_ws2, num_machines_ws3):
         '''Constructor for initiating SimPy simulation environment.'''
         self.env = env
         self.ws1 = simpy.Resource(env, capacity=num_machines_ws1)
+        self.ws2 = simpy.Resource(env, capacity=num_machines_ws2)
+        self.ws3 = simpy.Resource(env, capacity=num_machines_ws3)
 
     def step_a(self, lot):
         yield self.env.timeout(2)
+
+    def step_b(self, lot):
+        yield self.env.timeout(3)
+
+    def step_c(self, lot):
+        yield self.env.timeout(4)
 
 
 def start_lot(env, lot, factory):
@@ -31,10 +45,11 @@ def start_lot(env, lot, factory):
     current_lot_id = next(lot_id)
 
     # The first step has a step_arrival_time of when the lot was created.
-    first_step = 'STEP A'
+    step_list = ['STEP A', 'STEP B', 'STEP C']
     GlobalVariables.lot_status = pd.concat([GlobalVariables.lot_status,
-                                 pd.DataFrame({'lot_id':current_lot_id, 'step_name':first_step, 'step_arrival_time':env.now},index=[current_lot_id])])
+                                 pd.DataFrame({'lot_id':current_lot_id, 'step_name':step_list[0], 'step_arrival_time':env.now},index=[current_lot_id])])
 
+    # ws1, step_a
     with factory.ws1.request() as request:
         GlobalVariables.lot_status.at[current_lot_id, 'step_arrival_time'] = env.now # The lot gets in queue for the first process.
         yield request
@@ -42,8 +57,30 @@ def start_lot(env, lot, factory):
         yield env.process(factory.step_a(lot))
         GlobalVariables.lot_status.at[current_lot_id, 'process_end_time'] = env.now # After yield, the lot has finished processing.
 
-def run_factory(env, num_machines_ws1=1, lots_ready_at_time_zero=3, interarrival_time=2):
-    factory = Factory(env, num_machines_ws1)
+    GlobalVariables.lot_status = pd.concat([GlobalVariables.lot_status,
+                                    pd.DataFrame({'lot_id':current_lot_id, 'step_name':step_list[1], 'step_arrival_time':env.now},index=[current_lot_id])])
+
+    # ws2, step_b
+    with factory.ws2.request() as request:
+        GlobalVariables.lot_status.at[current_lot_id, 'step_arrival_time'] = env.now # The lot gets in queue for the first process.
+        yield request
+        GlobalVariables.lot_status.at[current_lot_id, 'process_start_time'] = env.now # After yield, the lot begins the process.
+        yield env.process(factory.step_b(lot))
+        GlobalVariables.lot_status.at[current_lot_id, 'process_end_time'] = env.now # After yield, the lot has finished processing.
+
+    GlobalVariables.lot_status = pd.concat([GlobalVariables.lot_status,
+                                    pd.DataFrame({'lot_id':current_lot_id, 'step_name':step_list[2], 'step_arrival_time':env.now},index=[current_lot_id])])
+
+    # ws3, step_c
+    with factory.ws3.request() as request:
+        GlobalVariables.lot_status.at[current_lot_id, 'step_arrival_time'] = env.now # The lot gets in queue for the first process.
+        yield request
+        GlobalVariables.lot_status.at[current_lot_id, 'process_start_time'] = env.now # After yield, the lot begins the process.
+        yield env.process(factory.step_c(lot))
+        GlobalVariables.lot_status.at[current_lot_id, 'process_end_time'] = env.now # After yield, the lot has finished processing.
+
+def run_factory(env, num_machines_ws1=1, num_machines_ws2=1, num_machines_ws3=1, lots_ready_at_time_zero=3, interarrival_time=2):
+    factory = Factory(env, num_machines_ws1, num_machines_ws2, num_machines_ws3)
 
     # The factory starts with some number of lots ready to start processing at the first step.
     for lot in range(lots_ready_at_time_zero):
