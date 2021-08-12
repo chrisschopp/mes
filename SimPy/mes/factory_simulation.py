7e1df1376f3d705f9a1b172c20fb2ff3d02e610a
Chris Schopp <christopher.d.schopp@gmail.com>
2021-08-12 19:02:03 -0400

    formatted with Black

--- SimPy/mes/factory_simulation.py
+++ SimPy/mes/factory_simulation.py
@@ -4,58 +4,61 @@ import simpy
 
 
 class GlobalVars:
-    '''Holds variables needed by other classes.
-    '''
-    lot_status_df = pd.DataFrame(columns=['lot_id', 'step_name', 'step_arrival_time', 'process_start_time', 'process_end_time'])
-    process_time = [
-                            ['STEP A', 1],
-                            ['STEP B', 2],
-                            ['STEP C', 3],
-                            ['STEP D', 4]
-                        ]
-    num_machines_at_ws = [
-                            {'ws1': 1},
-                            {'ws2': 1},
-                            {'ws3': 1}
-                        ]
+    """Holds variables needed by other classes.
+    """
+
+    lot_status_df = pd.DataFrame(
+        columns=[
+            "lot_id",
+            "step_name",
+            "step_arrival_time",
+            "process_start_time",
+            "process_end_time",
+        ]
+    )
+    process_time = [["STEP A", 1], ["STEP B", 2], ["STEP C", 3], ["STEP D", 4]]
+    num_machines_at_ws = [{"ws1": 1}, {"ws2": 1}, {"ws3": 1}]
 
 
 class Factory(object):
-    '''Holds the workstation(s) (i.e., SimPy Resource) and step that at which each Lot will be processed.
-    '''
+    """Holds the workstation(s) (i.e., SimPy Resource) and step that at which each Lot will be processed.
+    """
+
     def __init__(self, env):
-        '''Constructor for initiating SimPy simulation environment.
-        '''
+        """Constructor for initiating SimPy simulation environment.
+        """
         self.env = env
 
     def step(self, lot):
-        '''A Lot runs at a step for the amount of time set by process_time.
-        '''
+        """A Lot runs at a step for the amount of time set by process_time.
+        """
         yield self.env.timeout(GlobalVars.process_time[lot.step_sequence_number][1])
 
 
 def _generate_lot_id():
-    '''Generator that assigns an incremental number to each lot, starting with lot_id = 1.
-    '''
+    """Generator that assigns an incremental number to each lot, starting with lot_id = 1.
+    """
     lot_id = 1
     while True:
         yield lot_id
         lot_id += 1
 
+
 class Lot(object):
-    '''Holds variables specific to each Lot instance as it flows through steps in the Factory.
-    '''
+    """Holds variables specific to each Lot instance as it flows through steps in the Factory.
+    """
+
     def __init__(self):
         self.lot_id = next(_generate_lot_id())
         self.step_sequence_number = 0
 
 
 def insert_datetime_for(env, lot, lot_event):
-    '''Inserts event datetimes into the lot_status_df.
+    """Inserts event datetimes into the lot_status_df.
     
     Examples of events would be 'step_arrival_time',
     'process_start_time', and 'process_end_time'.
-    '''
+    """
     GlobalVars.lot_status_df.at[lot.index, str(lot_event)] = env.now
 
 
@@ -63,21 +66,32 @@ def start_lot(env, lot, factory):
     lot.index = [len(GlobalVars.lot_status_df)]
     # ws1
     # The first step has a step_arrival_time of when the lot was created.
-    GlobalVars.lot_status_df = pd.concat([GlobalVars.lot_status_df,
-                                            pd.DataFrame(
-                                                        {'lot_id': lot.lot_id,
-                                                        'step_name': GlobalVars.process_time[lot.step_sequence_number][0],
-                                                        'step_arrival_time': env.now},
-                                                        index=lot.index
-                                                        )
-                                            ])
+    GlobalVars.lot_status_df = pd.concat(
+        [
+            GlobalVars.lot_status_df,
+            pd.DataFrame(
+                {
+                    "lot_id": lot.lot_id,
+                    "step_name": GlobalVars.process_time[lot.step_sequence_number][0],
+                    "step_arrival_time": env.now,
+                },
+                index=lot.index,
+            ),
+        ]
+    )
 
     with factory.ws1.request() as request:
-        insert_datetime_for(env, lot, 'step_arrival_time') # The lot gets in queue for the first process.
+        insert_datetime_for(
+            env, lot, "step_arrival_time"
+        )  # The lot gets in queue for the first process.
         yield request
-        insert_datetime_for(env, lot, 'process_start_time') # After yield, the lot begins the process.
+        insert_datetime_for(
+            env, lot, "process_start_time"
+        )  # After yield, the lot begins the process.
         yield env.process(factory.step(lot))
-        insert_datetime_for(env, lot, 'process_end_time') # After yield, the lot has finished processing.
+        insert_datetime_for(
+            env, lot, "process_end_time"
+        )  # After yield, the lot has finished processing.
 
     lot.step_sequence_number += 1
     env.process(continue_lot(env, lot, factory))
@@ -86,21 +100,32 @@ def start_lot(env, lot, factory):
 def continue_lot(env, lot, factory):
     lot.index = [len(GlobalVars.lot_status_df)]
     # ws2
-    GlobalVars.lot_status_df = pd.concat([GlobalVars.lot_status_df,
-                                            pd.DataFrame(
-                                                        {'lot_id': lot.lot_id,
-                                                        'step_name': GlobalVars.process_time[lot.step_sequence_number][0],
-                                                        'step_arrival_time': env.now},
-                                                        index=lot.index
-                                                        )
-                                            ])
+    GlobalVars.lot_status_df = pd.concat(
+        [
+            GlobalVars.lot_status_df,
+            pd.DataFrame(
+                {
+                    "lot_id": lot.lot_id,
+                    "step_name": GlobalVars.process_time[lot.step_sequence_number][0],
+                    "step_arrival_time": env.now,
+                },
+                index=lot.index,
+            ),
+        ]
+    )
 
     with factory.ws2.request() as request:
-        insert_datetime_for(env, lot, 'step_arrival_time') # The lot gets in queue for the first process.
+        insert_datetime_for(
+            env, lot, "step_arrival_time"
+        )  # The lot gets in queue for the first process.
         yield request
-        insert_datetime_for(env, lot, 'process_start_time') # After yield, the lot begins the process.
+        insert_datetime_for(
+            env, lot, "process_start_time"
+        )  # After yield, the lot begins the process.
         yield env.process(factory.step(lot))
-        insert_datetime_for(env, lot, 'process_end_time') # After yield, the lot has finished processing.
+        insert_datetime_for(
+            env, lot, "process_end_time"
+        )  # After yield, the lot has finished processing.
 
     lot.step_sequence_number += 1
     env.process(continue_lot(env, lot, factory))
@@ -122,7 +147,10 @@ def run_factory(env, lots_ready_at_time_zero=3, interarrival_time=2):
     # The factory receives new lots according the the interarrival_time.
     while True:
         yield env.timeout(interarrival_time)
-        env.process(start_lot(env, Lot(), factory)) # After yield, a new Lot arrives at the factory.
+        env.process(
+            start_lot(env, Lot(), factory)
+        )  # After yield, a new Lot arrives at the factory.
+
 
 def get_lot_status_df(simulation_start_time):
     """Returns DataFrames of mes.hist and mes.current.
@@ -138,16 +166,27 @@ def get_lot_status_df(simulation_start_time):
     # Convert simulation minutes to datetimes starting at simulation_start_time.
     # Converts unit time to hours so we have a longer time period
     simulation_start_time = pd.Timestamp(year=year, month=month, day=day)
-    for event in ['step_arrival_time','process_start_time','process_end_time']:
-        mes_data[event] = simulation_start_time + pd.TimedeltaIndex(mes_data[event], unit='h').to_pytimedelta()
+    for event in ["step_arrival_time", "process_start_time", "process_end_time"]:
+        mes_data[event] = (
+            simulation_start_time
+            + pd.TimedeltaIndex(mes_data[event], unit="h").to_pytimedelta()
+        )
 
-    MES = namedtuple("mes", ["hist","current"])
-    mes = MES(mes_data[~mes_data['process_end_time'].isna()].copy(),
-            mes_data[mes_data['process_end_time'].isna()].copy())
+    MES = namedtuple("mes", ["hist", "current"])
+    mes = MES(
+        mes_data[~mes_data["process_end_time"].isna()].copy(),
+        mes_data[mes_data["process_end_time"].isna()].copy(),
+    )
 
-    mes.hist["queue_time_hours"] = (mes.hist["process_start_time"] - mes.hist["step_arrival_time"]) / pd.Timedelta(hours=1)
-    mes.hist["process_time_hours"] = (mes.hist["process_end_time"] - mes.hist["process_start_time"]) / pd.Timedelta(hours=1)
-    mes.hist["step_cycle_time_hours"] = (mes.hist["process_end_time"] - mes.hist["step_arrival_time"]) / pd.Timedelta(hours=1)
+    mes.hist["queue_time_hours"] = (
+        mes.hist["process_start_time"] - mes.hist["step_arrival_time"]
+    ) / pd.Timedelta(hours=1)
+    mes.hist["process_time_hours"] = (
+        mes.hist["process_end_time"] - mes.hist["process_start_time"]
+    ) / pd.Timedelta(hours=1)
+    mes.hist["step_cycle_time_hours"] = (
+        mes.hist["process_end_time"] - mes.hist["step_arrival_time"]
+    ) / pd.Timedelta(hours=1)
 
     return mes
 
