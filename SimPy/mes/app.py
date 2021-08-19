26d9a82711fd76b02a39ffd3f0b5dff8b5ca6065
Chris Schopp <christopher.d.schopp@gmail.com>
2021-08-18 20:39:41 -0400

    initial commit

---
+++ SimPy/mes/app.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+from factory_simulation import *
+
+
+def main():
+    # Create a text element and let the reader know the data is loading.
+    data_load_state = st.text("Running simulation...")
+    mes = simulation_config()
+    # Notify the reader that the data was successfully loaded.
+    data_load_state.text("Running simulation...done.")
+
+    st.sidebar.title("Factory Simulation")
+    app_mode = st.sidebar.selectbox(
+        "Choose the app mode", ["Instructions", "📄 MES history", "📄 MES current state"],
+    )
+    if app_mode == "Instructions":
+        st.write(welcome_message())
+        st.sidebar.success("Select an option above.")
+    elif app_mode == "📄 MES history":
+        st.write(mes.hist)
+    elif app_mode == "📄 MES current state":
+        st.write(mes.current)
+
+
+@st.cache
+def simulation_config():
+    load_simulation(
+        interarrival_time_scale=2, lots_ready_at_time_zero=3, random_seed=42
+    )
+    mes = create_mes_data(simulation_start_time=(2021, 1, 1))
+    return mes
+
+
+@st.cache
+def load_simulation(
+    interarrival_time_scale=2, lots_ready_at_time_zero=3, random_seed=42
+):
+    np.random.seed(42)
+    interarrival_time = np.random.exponential(
+        scale=interarrival_time_scale
+    )  # The scale parameter (lambda) for an exponential distribution is equal to both the mean and std dev.
+
+    env = simpy.Environment()
+    env.process(
+        run_factory(
+            env,
+            lots_ready_at_time_zero=lots_ready_at_time_zero,
+            interarrival_time=interarrival_time,
+        )
+    )
+    env.run(until=10)
+
+
+def welcome_message():
+    text = """
+    # Factory Simulation
+
+    Welcome to the simulation.
+    """
+    return text
+
+
+if __name__ == "__main__":
+    main()
